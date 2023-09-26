0fac3ad93c841cb4732cf266884a0517ca023d80
D0N4T024 <103001490+D0N4T024@users.noreply.github.com>
2023-09-26 14:25:12 -0400

    Pruebas con axios.get()

--- src/app/asignaturas/page.js
+++ src/app/asignaturas/page.js
@@ -63,7 +63,7 @@ export default Asignaturas;*/
 
 
 
-"use client"
+/*"use client"
 
 import * as React from 'react';
 import { useState, useEffect } from 'react';
@@ -100,6 +100,75 @@ const Asignaturas = () => {
 
   // DataGrid's rows
   const rows = people?.map((item, index) => ({
+    id: index,
+    name: item.Nombre_Usuario,
+    lastName: item.Apellido_Usuario,
+    rol: item.Rol,
+    userName: item.UserName,
+    email: item.Email,
+    state: item.Estado,
+
+  }));
+
+  // DataGrid creation
+  return (
+    <div style={{ height: 650, width: '100%', background: '#fff', fontSize: '20px' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+      />
+    </div>
+  );
+};
+
+export default Asignaturas;*/
+
+
+
+
+
+
+
+
+
+
+
+"use client"
+import axios from 'axios';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const Asignaturas = () => {
+  const [employeeList, setEmployeeList] =  useState([])
+
+  useEffect(() => {
+    getEmployeeList();
+  }, []);
+
+  const getEmployeeList = () => {
+    axios.get("https://localhost:44377/api/Usuarios").then((response) => {
+      
+      setEmployeeList(response.data);
+    }).catch((error) => {
+      console.error('Error al obtener la lista de empleados:', error);
+    }); 
+}
+
+  // DataGrid's columns
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 130 },
+    { field: 'name', headerName: 'Nombre', width: 130 },
+    { field: 'lastName', headerName: 'Apellido', width: 430 },
+    { field: 'rol', headerName: 'Rol', width: 130 },
+    { field: 'userName', headerName: 'Nombre de usuario', width: 130 },
+    { field: 'email', headerName: 'Email', width: 130 },
+    { field: 'state', headerName: 'Estado', width: 130 },
+  ];
+
+  // DataGrid's rows
+  const rows = employeeList.map((item, index) => ({
     id: index,
     name: item.Nombre_Usuario,
     lastName: item.Apellido_Usuario,
