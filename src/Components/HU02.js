d93f8bc9008cf1e366183526f3b96fa2f505621c
Sebastian Liriano Nova <1105207@est.intec.edu.do>
2023-09-27 21:09:03 -0400

    Pantalla nueva: Hice la pantalla cambiar contraseñ

---
+++ src/Components/HU02.js
@@ -0,0 +1,35 @@
+/* eslint-disable jsx-a11y/alt-text */
+"use client"
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Login = () => (
+  <section class="text-gray-600 body-font relative">
+    <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div class="lg:w-2/3 bg-sky-900 rounded-lg overflow-hidden sm:mr-16 mx-10 my-7 p-10 flex items-end justify-start relative">
+        <Image title="back"
+          width={2000}
+          height={2000}
+          src="/imagenes/GlassCore5.png"
+          objectFit='cover'>
+        </Image>
+      </div>
+      <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full] md: py-36 px-3 mx-10 md:mt-0">
+        <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Cambiar Contraseña</h2>
+        <div class="relative mb-4">
+          <label for="full-user" class="leading-7 text-sm text-gray-600">Contraseña nueva</label>
+          <input type="number" id="full-user" user="full-user" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />          </div>
+        <div class="relative mb-4">
+          <label for="password" class="leading-7 text-sm text-gray-600">Confirmar contraseña</label>
+          <input type="password" id="password" user="password" class="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+        </div>
+        <Link href="/Inicio">
+          <button class="text-white bg-sky-900 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Confirmar</button>
+        </Link>
+      </div>
+    </div>
+  </section>
+)
+
+export default Login
