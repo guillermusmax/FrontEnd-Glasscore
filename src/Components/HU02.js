abf2d36e0d4b2317c0dfeb34f937a1ed6ec05420
Sebastian Liriano Nova <1105207@est.intec.edu.do>
2023-09-27 23:37:10 -0400

    Probando a ver si la tipografia cambio de tamaño y fuente

--- src/Components/HU02.js
+++ src/Components/HU02.js
@@ -16,12 +16,12 @@ const HU02 = () => (
         </Image>
       </div>
       <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full] md: py-36 px-3 mx-10 md:mt-0">
-        <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Cambiar Contraseña</h2>
+      h2 style={{ fontSize: 'calc(36px + 2vmin)', letterSpacing: 'normal', width: '100%' }} <h2 class="text-customBlue text-lg font-inter font-black mb-5">Cambiar Contraseña</h2>
         <div class="relative mb-4">
           <label for="paswword" class="leading-7 text-sm text-customBlue">Contraseña nueva</label>
           <input type="password" id="password" user="password" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />          </div>
         <div class="relative mb-4">
-          <label for="password" class="leading-7 text-sm text-gray-600">Confirmar contraseña</label>
+          <label for="password" class="leading-7 text-sm text-customBlue">Confirmar contraseña</label>
           <input type="password" id="password" user="password" class="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
         <Link href="/Inicio">
