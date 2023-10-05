48a6c5b605274df8854f59f2ec1f7b95474de0c0
Sebastian Liriano Nova <1105207@est.intec.edu.do>
2023-10-05 18:56:28 -0400

    Creacion de layout docente

---
+++ src/layouts/DocenteUsuarioLayout/DocenteUsuarioLayout.js
@@ -0,0 +1,25 @@
+import styles from "./DocenteUsuarioLayout.module.scss";
+import { MenuBar } from "@/components/Layout/MenuBar/MenuBar";
+import { ContentAdmin } from "@/components/Layout/Content/Content";
+import { TopLogo } from "@/components/Layout/TopLogo/TopLogo"
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
+export function DocenteUsuarioLayout() {
+  const [menuVisible, setMenuVisible] = useState(true);
+  
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleSelectOption = (option) => {
+    setSelectedOption(option);
+  };
+
+
+
+  return (
+    <div >
+      <MenuBar menuVisible={menuVisible} setMenuVisible={setMenuVisible} onSelectOption={handleSelectOption}/>
+      <ContentAdmin menuVisible={menuVisible} selectedOption={selectedOption}/>
+    </div>
+  )
+}
