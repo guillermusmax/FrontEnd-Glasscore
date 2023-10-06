a269764ecd2738459dd6e81e34798acbc3ab21a1
Sebastian Liriano Nova <1105207@est.intec.edu.do>
2023-10-05 21:45:08 -0400

    Modificaciones para presentar mañana solamente

--- src/components/Layout/Seleccion/Seleccion.js
+++ src/components/Layout/Seleccion/Seleccion.js
@@ -1,14 +1,175 @@
+import Link from 'next/link';
 import styles from './Seleccion.module.scss';
 export function Seleccion(){
     return(
         <div>
-         <h1 className={styles.pagetitle}>selección</h1>
-         <div className="relative h-screen w-screen"> {/* Contenedor padre */}
-    <button className="flex justify-center items-center w-[152px] md:w-auto h-[39px] py-[8.5px] px-[36px] flex-shrink-0 rounded-[32px] bg-white shadow absolute left-[315px] top-[224px] md:left-auto md:top-auto">
-        Cancelar
-    </button>
-</div>
-
+         <h1 className={styles.pagetitle}>Selección</h1>
+         <div className="absolute top-[300px] left-[709px] bg-gainsboro w-0.5 h-0.5" />
+        <div className="absolute top-[855px] left-[1053px] shadow-[0px_4px_20px_#e8ecff] w-[352px] h-12 text-abupeteng">
+          <div className="absolute top-[0px] left-[0px] rounded-2xl bg-putih w-[352px] h-12" />
+          <div className="absolute top-[12px] left-[29px] w-[299px] h-6">
+            <img
+              className="absolute top-[0px] left-[275px] w-6 h-6"
+              alt=""
+              src="/vuesaxlinearsearchnormal.svg"
+            />
+            <div className="absolute top-[1px] left-[0px] leading-[22px] font-medium">
+              IDS346
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-[289px] left-[314px] w-[1048px] h-[18px] text-xs text-darkgray">
+          <div className="absolute top-[0px] left-[0px] font-semibold">{`Código `}</div>
+          <div className="absolute top-[0px] left-[194px] font-semibold">
+            Materia
+          </div>
+          <div className="absolute top-[0px] left-[349px] font-semibold">{`Sección `}</div>
+          <div className="absolute top-[3px] left-[519px] font-semibold">
+            Profesores
+          </div>
+          <div className="absolute top-[0px] left-[690px] font-semibold">{`Créditos `}</div>
+          <div className="absolute top-[0px] left-[846px] font-semibold">
+            Horario
+          </div>
+          <div className="absolute top-[0px] left-[976px] font-semibold">
+            Seleccionar
+          </div>
+        </div>
+        <div className="absolute top-[324px] left-[295px] w-[1110px] h-[675px]">
+          <div className="absolute top-[190px] left-[0px] w-[1110px] h-[85px]">
+            <div className="absolute top-[0px] left-[0px] rounded-lg bg-putih shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[1110px] h-[85px]" />
+            <div className="absolute top-[34px] left-[19px]">CBM-110</div>
+            <div className="absolute top-[34px] left-[176px]">
+              Mantequilla para la ing
+            </div>
+            <div className="absolute top-[34px] left-[385px]">02</div>
+            <div className="absolute top-[34px] left-[521px]">
+              Jugo de china
+            </div>
+            <div className="absolute top-[34px] left-[730px]">4</div>
+            <div className="absolute top-[25px] left-[810px] text-center">
+              <p className="m-0">Mar: 14/16, Mier: 18/20,</p>
+              <p className="m-0"> Jue: 20/22</p>
+            </div>
+            <img
+              className="absolute top-[33px] left-[1074px] w-4 h-[18px] overflow-hidden"
+              alt=""
+              src="/trash-1.svg"
+            />
+            <img
+              className="absolute top-[33px] left-[1022px] w-[19px] h-[19px] overflow-hidden"
+              alt=""
+              src="/pen-1.svg"
+            />
+          </div>
+          <div className="absolute top-[290px] left-[0px] w-[1110px] h-[85px]">
+            <div className="absolute top-[0px] left-[0px] rounded-lg bg-putih shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[1110px] h-[85px]" />
+            <div className="absolute top-[34px] left-[19px]">CBM-110</div>
+            <div className="absolute top-[34px] left-[176px]">
+              Mantequilla para la ing
+            </div>
+            <img
+              className="absolute top-[33px] left-[1074px] w-4 h-[18px] overflow-hidden"
+              alt=""
+              src="/trash-1.svg"
+            />
+            <img
+              className="absolute top-[33px] left-[1022px] w-[19px] h-[19px] overflow-hidden"
+              alt=""
+              src="/pen-1.svg"
+            />
+          </div>
+          <div className="absolute top-[386px] left-[0px] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[1110px] h-[85px]">
+            <div className="absolute top-[0px] left-[0px] rounded-lg bg-putih w-[1110px] h-[85px]" />
+            <div className="absolute top-[34px] left-[19px]">CBM-110</div>
+            <div className="absolute top-[34px] left-[176px]">
+              Mantequilla para la ing
+            </div>
+            <div className="absolute top-[34px] left-[385px]">02</div>
+            <div className="absolute top-[34px] left-[521px]">
+              Jugo de china
+            </div>
+            <div className="absolute top-[34px] left-[730px]">4</div>
+            <div className="absolute top-[25px] left-[810px] text-center">
+              <p className="m-0">Mar: 14/16, Mier: 18/20,</p>
+              <p className="m-0"> Jue: 20/22</p>
+            </div>
+            <img
+              className="absolute top-[33px] left-[1074px] w-4 h-[18px] overflow-hidden"
+              alt=""
+              src="/trash-1.svg"
+            />
+            <img
+              className="absolute top-[33px] left-[1022px] w-[19px] h-[19px] overflow-hidden"
+              alt=""
+              src="/pen-1.svg"
+            />
+          </div>
+          <div className="absolute top-[590px] left-[0px] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[1110px] h-[85px]">
+            <div className="absolute top-[0px] left-[0px] rounded-lg bg-lightblue w-[1110px] h-[85px]" />
+            <div className="absolute top-[34px] left-[19px]">IDS346</div>
+            <div className="absolute top-[34px] left-[176px] inline-block w-[482px]">
+              MODELOS Y METODOS
+            </div>
+            <div className="absolute top-[34px] left-[730px] inline-block w-[74px]">
+              4 creditos
+            </div>
+            <img
+              className="absolute top-[32.06px] left-[1064.12px] w-[19.89px] h-[19.89px]"
+              alt=""
+              src="/close.svg"
+            />
+          </div>
+          <div className="absolute top-[95px] left-[0px] w-[1110px] h-[85px]">
+            <div className="absolute top-[0px] left-[0px] rounded-lg bg-putih shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[1110px] h-[85px]" />
+            <div className="absolute top-[34px] left-[19px]">CBM-110</div>
+            <div className="absolute top-[34px] left-[176px]">
+              Mantequilla para la ing
+            </div>
+            <div className="absolute top-[34px] left-[385px]">02</div>
+            <div className="absolute top-[34px] left-[521px]">
+              Jugo de china
+            </div>
+            <div className="absolute top-[34px] left-[730px]">4</div>
+            <div className="absolute top-[25px] left-[810px] text-center">
+              <p className="m-0">Mar: 14/16, Mier: 18/20,</p>
+              <p className="m-0"> Jue: 20/22</p>
+            </div>
+            <img
+              className="absolute top-[33px] left-[1074px] w-4 h-[18px] overflow-hidden"
+              alt=""
+              src="/trash-1.svg"
+            />
+            <img
+              className="absolute top-[33px] left-[1022px] w-[19px] h-[19px] overflow-hidden"
+              alt=""
+              src="/pen-1.svg"
+            />
+          </div>
+          <div className="absolute top-[0px] left-[0px] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[1110px] h-[85px]">
+            <div className="absolute top-[0px] left-[0px] rounded-lg bg-putih w-[1110px] h-[85px]" />
+            <div className="absolute top-[34px] left-[19px]">CBM-110</div>
+            <div className="absolute top-[36px] left-[176px]">
+              Cálculo Vectorial
+            </div>
+            <div className="absolute top-[28px] left-[385px]">01</div>
+            <div className="absolute top-[31px] left-[521px]">
+              Jugo de china
+            </div>
+            <div className="absolute top-[31px] left-[730px]">4</div>
+            <div className="absolute top-[34px] left-[852px]">Mar: 12/14</div>
+            <img
+              className="absolute top-[33px] left-[1074px] w-4 h-[18px] overflow-hidden"
+              alt=""
+              src="/trash-1.svg"
+            />
+            <img
+              className="absolute top-[33px] left-[1022px] w-[19px] h-[19px] overflow-hidden"
+              alt=""
+              src="/pen-1.svg"
+            />
+          </div>
+        </div>
         </div>
     )
 }
