4765ee635891be964fe1117de4e115ec471e3eed
Sebastian Liriano Nova <1105207@est.intec.edu.do>
2023-10-06 10:22:52 -0400

    Modificaciones

--- src/components/Layout/AsignaturaSeleccion/AsignaturaSeleccion.js
+++
@@ -1,7 +0,0 @@
-export function AsignaturaSeleccion(){
-    return(
-        <div>
-           AsignaturaSeleccion
-        </div>
-    )
-}
--- src/components/Layout/BuscarAsignacion/BuscarAsignacion.js
+++ src/components/Layout/BuscarAsignacion/BuscarAsignacion.js
@@ -1,5 +1,6 @@
 import { Modal, Form, Button, Checkbox, Segment, Table, Icon, Pagination} from "semantic-ui-react";
-import styles from './BuscarAsignacion.module.scss';
+import { AñadirAsignatura } from '@/components/Layout/AñadirAsignatura/AñadirAsignatura';
+import styles from './BuscarAsignacion.module.scss';  './AñadirAsignatura.module.scss'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -12,6 +13,15 @@ let data = [
 
 export function BuscarAsignacion() {
 
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  }
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  }
+  
   const [Asignaturas, setAsignaturas] =  useState([])
   useEffect(() => {
     getUserList();
@@ -25,31 +35,6 @@ export function BuscarAsignacion() {
       console.error('Error al obtener la lista de usuarios:', error);
     });
   }
-
-    const [confirmOpen, setConfirmOpen] = useState(false);
-    const [selectedItemId, setSelectedItemId] = useState(null);
-
-  const handleEdit = (id) => {
-    // Lógica para editar el elemento con el ID proporcionado
-    // Aquí puedes abrir un modal o realizar la acción que necesites.
-  };
-
-  const handleDelete = (id) => {
-    setSelectedItemId(id);
-    setConfirmOpen(true);
-  };
-
-  const handleConfirmDelete = () => {
-    // Lógica para eliminar el elemento con el ID seleccionado
-    // Aquí debes eliminar el elemento de tu lista de datos.
-    // Por ejemplo, puedes usar filter para crear una nueva lista sin el elemento.
-    data = data.filter((item) => item.id !== selectedItemId);
-    setConfirmOpen(false);
-  };
-
-  const handleCancelDelete = () => {
-    setConfirmOpen(false);
-  };
 
   const itemsPerPage = 10;
   const totalPages = Math.ceil(data.length / itemsPerPage);
@@ -81,7 +66,7 @@ export function BuscarAsignacion() {
               <Table.Cell style={{ width: '200px' }} className={styles.centeredCell}>{item.Materia}</Table.Cell>
               <Table.Cell style={{ width: '200px' }} className={styles.centeredCell}>{item.Créditos}</Table.Cell>
               <Table.Cell style={{ width: '100px' }} className={styles.centeredCell}>
-              <Button icon color="" onClick={() => handleEdit(item.id)} className={styles.iconButton}>
+              <Button icon color="" onClick={item} className={styles.iconButton}>
                   <Icon name="add" />
                 </Button>
               </Table.Cell>
@@ -94,7 +79,7 @@ export function BuscarAsignacion() {
         totalPages={totalPages}
         onPageChange={handlePageChange}
       />
-
+              <AñadirAsignatura isOpen={modalOpen} onClose={handleCloseModal} />
     </div>
   )
 }
