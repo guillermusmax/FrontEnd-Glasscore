905c0f2f14c1c12641e9e0e493b88e2e94212d00
Sebastian Liriano Nova <1105207@est.intec.edu.do>
2023-10-05 20:53:48 -0400

    Pequeño desarrollo en la creacion de seleccion

--- src/components/Layout/Seleccion/Seleccion.js
+++ src/components/Layout/Seleccion/Seleccion.js
@@ -1,7 +1,14 @@
+import styles from './Seleccion.module.scss';
 export function Seleccion(){
     return(
         <div>
-            seleccion
+         <h1 className={styles.pagetitle}>selección</h1>
+         <div className="relative h-screen w-screen"> {/* Contenedor padre */}
+    <button className="flex justify-center items-center w-[152px] md:w-auto h-[39px] py-[8.5px] px-[36px] flex-shrink-0 rounded-[32px] bg-white shadow absolute left-[315px] top-[224px] md:left-auto md:top-auto">
+        Cancelar
+    </button>
+</div>
+
         </div>
     )
 }
